cc9811b0fe45aa72b63ca8973604231c7d986661
iknemis <marianacarto.94@gmail.com>
2021-08-15 14:42:35 -0500

    final

--- C2_Reto2/src/Portatil.java
+++ C2_Reto2/src/Portatil.java
@@ -1,38 +1,38 @@
 public class Portatil extends Computador{
     // Con extends se heredan los métodos y atributos de
     // Atributos
-    private boolean tarjeraDeVideo = false;
+    private boolean tarjetaDeVideo = false;
     //Getter
-    public boolean isTarjeraDeVideo() {
-        return tarjeraDeVideo;
+    public boolean isTarjetaDeVideo() {
+        return tarjetaDeVideo;
     }
     //Setter
-    public void setTarjeraDeVideo(boolean tarjeraDeVideo) {
-        this.tarjeraDeVideo = tarjeraDeVideo;
+    public void setTarjetaDeVideo(boolean tarjetaDeVideo) {
+        this.tarjetaDeVideo = tarjetaDeVideo;
     }
     
     //Constructores
     public Portatil(double precioBase, boolean tarjetaDeVideo){
         this.setPrecioBase(precioBase);
-        this.setTarjeraDeVideo(tarjeraDeVideo);
+        this.setTarjetaDeVideo(tarjetaDeVideo);
         }
     public Portatil(double pantalla, double precioBase, boolean tarjetaDeVideo){
         this.setPantalla(pantalla);
         this.setPrecioBase(precioBase);
-        this.setTarjeraDeVideo(tarjeraDeVideo);
+        this.setTarjetaDeVideo(tarjetaDeVideo);
         }
     public Portatil(double ano, double pantalla, double precioBase, boolean tarjetaDeVideo){
         this.setAno(ano);
         this.setPantalla(pantalla);
         this.setPrecioBase(precioBase);
-        this.setTarjeraDeVideo(tarjeraDeVideo);
+        this.setTarjetaDeVideo(tarjetaDeVideo);
         }
 
     // Métodos
     // Método para calcular precio de un computador Portatil 
     public double calcularPrecio(){
         double factor; // El precio varia en función de si tiene o no tarjeta de video
-        if (this.isTarjeraDeVideo()){
+        if (this.isTarjetaDeVideo()){
             factor = 1.2;
         } else {
             factor = 1;
--- C2_Reto2/src/PrecioTotal.java
+++ C2_Reto2/src/PrecioTotal.java
@@ -13,13 +13,24 @@ public class PrecioTotal {
     public void mostrarTotales(){
         
         for (int x = 0; x < listaComputadores.length; x++){
+            
+            if (this.listaComputadores[x] instanceof Computador){
+                this.totalComputadores = this.totalComputadores + this.listaComputadores[x].calcularPrecio();
+            }
 
-            System.out.println("clase de "+x+" : "+listaComputadores[x].getClass()+" y su Precio Total es " +this.listaComputadores[x].calcularPrecio());
+            if(this.listaComputadores[x] instanceof Portatil){
+                this.totalPortatiles = this.totalPortatiles + this.listaComputadores[x].calcularPrecio();
+            }
+
+            if(this.listaComputadores[x] instanceof DeMesa){
+                this.totalDeMesa = this.totalDeMesa + this.listaComputadores[x].calcularPrecio();
+            }
         }
 
-        System.out.println("El precio total de los Computadores es de "+totalComputadores);
-        System.out.println("La suma del precio de los Portatiles es de "+totalPortatiles);
-        System.out.println("La suma del precio de los Computadores de Mesa es de "+totalDeMesa);
+        System.out.println("La suma del precio de los computadores es de "+this.totalComputadores);
+        System.out.println("La suma del precio de los portatiles es de "+totalPortatiles);
+        System.out.println("La suma del precio de los de mesa es de "+totalDeMesa);
+
     }
 }
 
